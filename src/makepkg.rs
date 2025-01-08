ada80ba49fcacc3e1db3cc537a339b418a720663
RadicalTray <hydrochloricmahn@gmail.com>
2025-01-08 23:55:37 +0700

    change makepkg wrapper mod

--- src/makepkg.rs
+++ src/makepkg.rs
@@ -1,7 +1,7 @@
 use std::path::Path;
-use std::process::{Command, ExitStatus};
+use std::process::{Command, ExitStatus, Output};
 
-pub struct MakepkgOpts {
+pub struct Makepkg {
     nobuild: bool,
     noextract: bool,
     needed: bool,
@@ -11,9 +11,9 @@ pub struct MakepkgOpts {
     clean: bool,
 }
 
-impl Default for MakepkgOpts {
-    fn default() -> MakepkgOpts {
-        MakepkgOpts {
+impl Default for Makepkg {
+    fn default() -> Makepkg {
+        Makepkg {
             nobuild: false,
             noextract: false,
             needed: false,
@@ -25,39 +25,57 @@ impl Default for MakepkgOpts {
     }
 }
 
-/// will set cwd to path
-pub fn makepkg<P: AsRef<Path>>(path: P, flags: MakepkgOpts) -> ExitStatus {
-    Command::new("makepkg")
-        .args(get_args(flags))
-        .current_dir(path)
-        .status()
-        .expect("can't run makepkg")
-}
-
-// NOTE: Is there a way to write this better in rust?
-fn get_args(flags: MakepkgOpts) -> Vec<String> {
-    let mut args = Vec::new();
-    if flags.needed {
-        args.push("--needed");
-    }
-    if flags.force {
-        args.push("--force");
-    }
-    if flags.nobuild {
-        args.push("--nobuild");
-    }
-    if flags.noextract {
-        args.push("--noextract");
-    }
-    if flags.packagelist {
-        args.push("--packagelist");
-    }
-    if flags.cleanbuild {
-        args.push("--cleanbuild");
-    }
-    if flags.clean {
-        args.push("--clean");
+impl Makepkg {
+    pub fn new() -> Makepkg {
+        Makepkg {
+            ..Default::default()
+        }
     }
 
-    args.iter().map(|x| x.to_string()).collect()
+    pub fn status<P: AsRef<Path>>(&self, cwd: P) -> ExitStatus {
+        Command::new("makepkg")
+            .args(self.get_args())
+            .current_dir(cwd)
+            .status()
+            .expect("can't run makepkg")
+    }
+
+    pub fn output<P: AsRef<Path>>(&self, cwd: P) -> Output {
+        Command::new("makepkg")
+            .args(self.get_args())
+            .current_dir(cwd)
+            .output()
+            .expect("can't run makepkg")
+    }
+
+    // NOTE: Is there a way to write this better in rust?
+    //
+    // PERF: this practically makes 2 vec if compiler doesn't see it
+    fn get_args(&self) -> Vec<String> {
+        let mut args = Vec::new();
+        let flags = self;
+        if flags.needed {
+            args.push("--needed");
+        }
+        if flags.force {
+            args.push("--force");
+        }
+        if flags.nobuild {
+            args.push("--nobuild");
+        }
+        if flags.noextract {
+            args.push("--noextract");
+        }
+        if flags.packagelist {
+            args.push("--packagelist");
+        }
+        if flags.cleanbuild {
+            args.push("--cleanbuild");
+        }
+        if flags.clean {
+            args.push("--clean");
+        }
+
+        args.iter().map(|x| x.to_string()).collect()
+    }
 }
